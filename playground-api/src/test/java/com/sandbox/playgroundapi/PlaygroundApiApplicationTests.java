285323debf257180f6b87ef67492bf1aadf1de9c
유승우/디지털채널서비스팀/HAE <seungwoo@hyundai-autoever.com>
2026-01-12 14:46:06 +0900

    api, security 서브 모듈 생성, root 설정

---
+++ playground-api/src/test/java/com/sandbox/playgroundapi/PlaygroundApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.playgroundapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ playground-security/src/test/java/com/sandbox/playgroundsecurity/PlaygroundSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.playgroundsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
