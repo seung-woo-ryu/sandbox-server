ab08b52e055e4c49d0c5445149d58e2b7be98c67
유승우/디지털채널서비스팀/HAE <seungwoo@hyundai-autoever.com>
2026-01-20 11:10:12 +0900

    refactor: remove unsued class

--- common-security/src/main/java/com/sandbox/commonsecurity/config/PropertiesConfig.java
+++
@@ -1,9 +0,0 @@
-package com.sandbox.commonsecurity.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(JwtProperties.class)
-public class PropertiesConfig {
-}
